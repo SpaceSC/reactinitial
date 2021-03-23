1216dbba7fe1ad1358fcab7cd9dbba1634e05ec3
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-23 17:07:50 +0100

    Set subscribe button to disabled until the input is valid

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function Subscription() {
+  const [ valid, setValid ] = useState(false);
+  const [ userEmail, setUserEmail ] = useState("");
+
+  function validate(e) {
+    setUserEmail(e.target.value);
+    if(e.target.value.includes(".") && e.target.value.includes("@")){
+      setValid(true);
+    }else{
+      setValid(false);
+    }
+  }
+
   return (
     <div>
       <form>
-      <input type="email" id="email" name="email"/>
-      <button >Subscribe</button>
+      <input type="email" id="email" name="email" onInput={validate}/>
+      {valid ? <button>Subscribe</button> : <button disabled>Subscribe</button>}
       </form>
     </div>
   )
