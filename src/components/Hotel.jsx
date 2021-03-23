98226c551db089584b32b436066b9d005538d8b9
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-23 16:54:22 +0100

    Add <Subscription /> and form

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from "react";
+import Subscription from "./Subscription";
 
 function Hotel({hotel}) {
   const [ show, setShow ] = useState(false);
+  const [subscribe, setSubscribe] = useState(false);
+  
 
   return (
     <div>
       <p>{hotel.name}</p>
-      {show && <p>{hotel.city} ({hotel.stars})</p>}
       {show ? <button onClick={() => {setShow(false)}}>Show less</button>: <button onClick={() => {setShow(true)}}>Show more</button>}
+      {show && <p>{hotel.city} ({hotel.stars})</p>}
+      {show && <button onClick={() => {setSubscribe(true)}}>Request more info</button>}
+      {subscribe && 
+      <div>
+        <h2>Request more info about {hotel.name}</h2>
+        <Subscription />
+      </div>
+      }
+      
     </div>
   )
 }
---
+++ src/components/Subscription.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Subscription() {
+  return (
+    <div>
+      <form>
+      <input type="email" id="email" name="email"/>
+      <button >Subscribe</button>
+      </form>
+    </div>
+  )
+}
+
+export default Subscription;
