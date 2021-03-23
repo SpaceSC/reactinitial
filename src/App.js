f5a4edb4e2d53611b6317539faa983929b427dc0
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-23 15:12:10 +0100

    Add h1, create <LoadingMask />, make api call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import './App.css'
+import React, { useState, useEffect } from "react";
+import LoadingMask from './components/LoadingMask';
 
 const App = () => {
 
+  const [ isLoaded, setIsLoaded ] = useState(false);
+
+  useEffect(() => {
+    fetch("api/hotels")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+      },
+      (error) => {
+        setError(true);
+        setIsLoaded(true);
+      }
+    )
+  }, [])
+
   return (
     <div className="App">
-      Todo...
+      <h1>Hotels</h1>
+      {!isLoaded && <LoadingMask/>}
     </div>
   )
 }
