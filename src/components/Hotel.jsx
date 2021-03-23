84d4dd11ed41bd93b502a14ad8b29c6e56c2fe8d
SpaceSC <ceciliasandor.22@gmail.com>
2021-03-23 16:20:59 +0100

    Add button to show/hide details of hotels

--- src/components/Hotel.jsx
+++ src/components/Hotel.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
 function Hotel({hotel}) {
+  const [ show, setShow ] = useState(false);
+
   return (
     <div>
       <p>{hotel.name}</p>
+      {show && <p>{hotel.city} ({hotel.stars})</p>}
+      {show ? <button onClick={() => {setShow(false)}}>Show less</button>: <button onClick={() => {setShow(true)}}>Show more</button>}
     </div>
   )
 }
